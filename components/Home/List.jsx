20eb54ec2d74e4b61ed0de5714bf5fef67391f22
Nathanael-R <nathanielwealth20@gmail.com>
2023-10-14 23:00:56 +0100

    Made list comp

--- components/Home/List.jsx
+++ components/Home/List.jsx
@@ -7,13 +7,14 @@ const List = () => {
   return (
     <View className="pt-10">
       <View>
-        <Pressable className="flex-row items-center gap-x-1 border-2 py-1 px-2 w-32 rounded-full">
+        <Pressable className="flex-row items-center border-2 py-1 px-2 w-32 rounded-full mb-2" style={{columnGap: 10}}>
           <Text className="text-base font-semibold">This Week</Text>
-          <Entypo name="chevron-thin-down" size={25} />
+          <Entypo name="chevron-thin-down" size={20} />
         </Pressable>
       </View>
-      <View>
-        <Image source={require('../../assets/stickman.png')} className="h-full"/>
+      <View className="items-center justify-center">
+        <Image source={require('../../assets/stickman.png')} className="h-40 w-full mb-4"/>
+        <Text className="font-medium text-base">You have no plans for this week</Text>
       </View>
     </View>
   );
